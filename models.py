c83f85f43a7cb9741225b4a08fcc7f3ffe0ff45c
Your Name <your-email@example.com>
2025-03-23 05:01:29 +0530

    this

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from sqlalchemy import Column, String
-from .database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    phone_number = Column(String, primary_key=True, index=True, unique=True)
-    hashed_password = Column(String)
